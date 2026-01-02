d4efd49614de4c274aab43c3e9aaec676c024aec
Uriyo <kgang6434@gmail.com>
2026-01-02 17:51:58 +0530

    added origins

--- src/server.py
+++ src/server.py
@@ -27,7 +27,11 @@ app.add_middleware(LoggingMiddleware)
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://www.ragment.in",
+        "https://ragment.in",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
